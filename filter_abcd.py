bf1b3e5399d09d28d579fb27b12bfc61cf1343d3
Carlos Rodriguez <45697881+carlosivanr@users.noreply.github.com>
2021-11-03 09:24:41 -0600

    Update filter_abcd.py

--- filter_abcd.py
+++ filter_abcd.py
@@ -4,7 +4,7 @@
 Author : Carlos Rodriguez <crodriguez@mrn.org>
 Date   : 11/02/2021
 Purpose: Filters ABCD study .txt files to match a pre-existing list of subjectkeys (GUIDs).
-Formats: Requires a list of subjectkeys in a .csv file with a header. Subjectkeys need to be written as NDAR_INV?????????.
+Formats: Requires a list of subjectkeys in a .csv file with a header. Subjectkeys need to be written as NDAR_INVxxxxxxxxx.
 Usage: python filter_abcd_py -p /path/to/.txt/files -f abcd_tlfb01.txt cct.txt -e baseline_year_1_arm_1 1_year_follow_up_y_arm_1 -s subjectkeys.csv
 """
 
@@ -22,7 +22,7 @@ parser.add_argument(
     "-p",
     "--path",
     metavar = "",
-    help = "Filepath where abcd .txt files are located",
+    help = "Filepath where abcd .txt files are located.",
     type = str,
     required = True,
 )
@@ -41,7 +41,7 @@ parser.add_argument(
     "-e",
     "--event_names",
     metavar = "",
-    help = "Event names to filter, defaults to baseline",
+    help = "Event names to filter, defaults to baseline_year_1_arm_1.",
     nargs = "+",
     type = str,
     default = "baseline_year_1_arm_1",
@@ -60,7 +60,7 @@ parser.add_argument(
     "-o",
     "--outpath",
     metavar = "",
-    help = "Filepath to output filtered files, defaults to cwd",
+    help = "Filepath to output filtered files, defaults to the current working directory.",
     type = str,
     default = os.getcwd(),
 )
@@ -74,7 +74,6 @@ subjects = list(subjects.iloc[:,0])
 
 for i in range(len(args.files)):
     file_name = args.files[i]
-    #print(file_name)
     abcd = pd.read_csv(args.path + '/' + file_name, 
                      sep = '\t', low_memory = False)
     sub_index = abcd.subjectkey.isin(subjects)
